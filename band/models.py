7af5c960f3e220596adb14082cfaaa2b376a8a43
banywick <android19377@gmail.com>
2024-10-01 11:18:49 +0300

    Новые фото, описание группы, деплой

--- band/models.py
+++ band/models.py
@@ -28,13 +28,13 @@ class Video(models.Model):
 
 
 class Photo(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Название')
+    title = models.CharField(null=True, blank=True, max_length=255, verbose_name='Название')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
     image = models.ImageField(upload_to='photos/', verbose_name='Фото')
     order = models.PositiveIntegerField(default=0, blank=False, null=False)
 
     def __str__(self):
-        return self.title
+        return ''if self.title is None else self.title
     
     class Meta:
         verbose_name = 'Фото'
