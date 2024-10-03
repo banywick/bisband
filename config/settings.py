fee46292c9e632767851768bad305f2070da0a99
Banywick <android19377@gmail.com>
2024-10-03 12:26:11 +0300

    Добавил домен и гуникорн

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['87.228.27.155', '127.0.0.1']
+ALLOWED_HOSTS = ['bisband.ru', '127.0.0.1']
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
