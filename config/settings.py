975373c877aa28c6b77428d3b54c6079da5cb2a4
Banywick <android19377@gmail.com>
2024-10-06 15:31:52 +0300

    настройка аловед хостс

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['bisband.ru', '127.0.0.1']
+ALLOWED_HOSTS = ['bisband.ru', 'www.bisband.ru']
+
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
