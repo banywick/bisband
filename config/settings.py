9d45d520593970ce65958e5a3d16872dbf066bd9
Banywick <android19377@gmail.com>
2024-10-07 16:16:14 +0300

    settings headers del

--- config/settings.py
+++ config/settings.py
@@ -33,10 +33,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['bisband.ru', 'www.bisband.ru']
 
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
